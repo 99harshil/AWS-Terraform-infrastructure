ff704f602276c5c4d34948e090e066e9c7c37f25
Harshil Thakkar <99harshil@gmail.com>
2023-11-08 23:25:35 -0500

    vpc module added

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,59 @@
+output "vpc_id" {
+  description = "The id of the VPC."
+  value       = "${aws_vpc.vpc.id}"
+}
+
+output "vpc_cidr" {
+  description = "The CDIR block used for the VPC."
+  value       = "${aws_vpc.vpc.cidr_block}"
+}
+
+output "internet_gateway" {
+  description = "Intenet Gateway id for allowing elements to access internet"
+  value       = "${aws_internet_gateway.internet_gateway.id}"
+}
+
+output "public_subnets" {
+  description = "A list of the public subnets."
+  value       = ["${aws_subnet.public_subnet.*.id}"]
+}
+
+output "private_subnets" {
+  description = "A list of the private subnets."
+  value       = ["${aws_subnet.private_subnet.*.id}"]
+}
+
+output "availability_zones" {
+  description = "List of the availability zones."
+  value       = "${var.availability_zones[var.aws_region]}"
+}
+
+output "main_route_table_id" {
+  description = "Id of main route table"
+  value       = "${aws_vpc.vpc.default_route_table_id}"
+}
+
+output "public_route_table_id" {
+  description = "Id of public route table"
+  value       = "${aws_route_table.public.id}"
+}
+
+output "private_route_table_id" {
+  description = "Id of private route table"
+  value       = "${aws_route_table.private.id}"
+}
+
+output "nat_gateway_id" {
+  description = "ID of NAT Gateway"
+  value       = aws_nat_gateway.nat.id
+}
+
+output "public_nacl_id" {
+  description = "Id of public Nacl"
+  value       = aws_network_acl.public.id
+}
+
+output "private_nacl_id" {
+  description = "Id of private Nacl"
+  value       = aws_network_acl.private.id
+}
---
+++ terraform/modules/vpc/route_table.tf
@@ -0,0 +1,68 @@
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_vpc.vpc.id]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["${var.name}-*-public"]
+  }
+} 
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_vpc.vpc.id]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["${var.name}-*-private"]
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  tags = { 
+    Name = "${var.name}-public-route-table"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.name}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(data.aws_subnets.public.ids)
+  subnet_id      = element(data.aws_subnets.public.ids, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(data.aws_subnets.private.ids)
+  subnet_id      = element(data.aws_subnets.private.ids, count.index)
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.internet_gateway.id}"
+
+  timeouts {
+    create = "5m"
+  }
+}
+
+resource "aws_route" "priavte_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_nat_gateway.nat.id}"
+
+  timeouts {
+    create = "5m"
+  }
+}
+
